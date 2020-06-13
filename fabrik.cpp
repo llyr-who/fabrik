3fa0faf1c6aed7af6b31c3511e1aca1fa293b669
llyr-who <jamesbushnell1992@gmail.com>
2020-06-13 14:46:17 +0100

    add mouse tracking

--- fabrik.cpp
+++ fabrik.cpp
@@ -59,16 +59,6 @@ static const char* fragment_shader_text =
     "    gl_FragColor = vec4(color, 1.0);\n"
     "}\n";
 
-static void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action,
-                         int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
 namespace fbk {
 
 typedef unsigned int uint;
@@ -80,20 +70,48 @@ public:
     void redraw();
     void run();
 
+    static void error_callback(int error, const char* description);
+
+    static void key_callback(GLFWwindow* window, int key, int scancode,
+                             int action, int mods);
+
+    static void mouse_callback(GLFWwindow* window, int button, int action,
+                               int mods);
+
+    static void mouse_move_callback(GLFWwindow* window, double xpos,
+                                    double ypos);
+
 private:
     void build_cloth_geometry_buffers();
-    uint vao_cloth_;
     cloth cloth_;
     GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+    GLuint cloth_buffer_;
 };
+
+void scene::error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void scene::key_callback(GLFWwindow* window, int key, int scancode, int action,
+                         int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void scene::mouse_callback(GLFWwindow* window, int button, int action,
+                           int mods) {}
+
+void scene::mouse_move_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        std::cout << xpos << " " << ypos << std::endl;
+    }
+}
 
 bool scene::init() {
     glfwSetErrorCallback(error_callback);
-
     if (!glfwInit()) exit(EXIT_FAILURE);
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
@@ -104,40 +122,17 @@ bool scene::init() {
     }
 
     glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_callback);
+    glfwSetCursorPosCallback(window, mouse_move_callback);
 
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-    // NOTE: OpenGL error checks have been omitted for brevity
-
+    // load extentions
     load();
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glGenBuffers(1, &cloth_buffer_);
+    glBindBuffer(GL_ARRAY_BUFFER, cloth_buffer_);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
-
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
-
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
-
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
-
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)(sizeof(float) * 2));
 }
 
 void scene::run() {
@@ -148,15 +143,8 @@ void scene::run() {
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float)height;
+        glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
 
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
@@ -165,7 +153,6 @@ void scene::run() {
 
     std::cout << "complete" << std::endl;
     glfwDestroyWindow(window);
-
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
